c3a1223ed90e6b53e1645b4372a9efd58026bfcd
Michael Emmanuel <michael.emmanuel732@gmail.com>
2024-04-02 19:59:28 -0400

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React from 'react';
+
+let currentYear = new Date().getFullYear();
 
 export default function Footer() {
   return (
-    <footer className="mb-10 px-4 text-center text-gray-500">
-      <small className="mb-2 block text-xs">
-        &copy; 2023 All rights reserved.
+    <footer className='mb-10 px-4 text-center text-gray-500'>
+      <small className='mb-2 block text-xs'>
+        &copy; currentYear All rights reserved.
       </small>
       {/* <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
