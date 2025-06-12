d4480771c90fbd469afe65375969128c2c242454
Michael Emmanuel <michael.emmanuel732@gmail.com>
2025-06-11 23:33:22 -0400

    text

--- components/contact.tsx
+++ components/contact.tsx
@@ -32,7 +32,7 @@ export default function Contact() {
       <SectionHeading>Contact me</SectionHeading>
 
       <p className='text-gray-700 -mt-6 dark:text-white/80'>
-        Please contact me directly at{' '}
+        Contact me directly at{' '}
         <a className='underline' href='mailto:michael.emmanuel732@gmail.com'>
           michael.emmanuel732@gmail.com
         </a>{' '}
